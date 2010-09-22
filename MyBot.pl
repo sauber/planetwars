75d58e53cdf9730b54229143ac8881139a67511d
Soren Dossing <netcom@sauber.net>
2010-09-22 16:43:26 +0900

    Send 6 from each planet to closest not owned -- Soren

--- MyBot.pl
+++ MyBot.pl
@@ -7,12 +7,24 @@ use POSIX;
 
 local $| = 1;
 my $map_data;
+my $distances;
+my $closest;
+
+sub x {
+ use Data::Dumper;
+ warn Data::Dumper->Dump([$_[1]], ["*** $_[0]"]);
+}
+
 
 while(1) {
     my $current_line = <STDIN>;
     if ($current_line =~ m/go/) {
         my $pw = new PlanetWars($map_data);
-        DoTurn($pw);
+        unless ( $distances ) {
+          $distances = CalcDistances($pw);
+          $closest = OrderNeighbors($pw, $distances);
+        }
+        DoTurn($pw, $distances, $closest);
         $pw->FinishTurn();
         $map_data = [];
     } elsif ($current_line eq "stop\n") {
@@ -22,7 +34,11 @@ while(1) {
     }
 }
 sub DoTurn {
-    my ($pw) = @_;
+    my ($pw,$distance,$neighbor) = @_;
+
+    # Just send to closest for now
+    SendToClosest($pw,$distance,$neighbor);
+    return;
 
       # (1) If we currently have a fleet in flight, just do nothing.
     #if ($pw->MyFleets() > 0) {
@@ -62,4 +78,70 @@ sub DoTurn {
         my $num_ships = $source_num_ships / 2;
         $pw->IssueOrder($source,$dest,ceil($num_ships));
     }
+}
+
+# Send fleets to closest planets
+#
+sub SendToClosest {
+  my($pw,$distance,$neighbor) = @_;
+
+  # Send 6 ships to closet planet that is not mine
+  my %not_my_planets = map {( $_->PlanetID() => 1 )} $pw->NotMyPlanets();
+  for my $myplanet ( $pw->MyPlanets() ) {
+    next unless $myplanet->NumShips() > 6;
+    my $myplanetid = $myplanet->PlanetID();
+    for my $neighbor ( @{ $neighbor->{$myplanetid} } ) {
+      my $neighborid = $neighbor->PlanetID();
+      next unless $not_my_planets{$neighborid};
+      $pw->IssueOrder($myplanetid,$neighborid,6);
+      last;
+    }
+  }
+}
+
+sub CalcDistances {
+  my($pw) = @_;
+  my $distance;
+
+  my $count = 0;
+  my @planetids = map $_->PlanetID(), $pw->Planets;
+  for my $p1 ( @planetids ) {
+    for my $p2 ( @planetids ) {
+      next if $p1 == $p2;
+      my @index = sort { $a <=> $b } ( $p1, $p2 );
+      #$distance->{$index[0]}{$index[1]} ||= $pw->Distance( @index );
+      next if $distance->{$index[0]}{$index[1]};
+      $distance->{$index[0]}{$index[1]} = $pw->Distance( @index );
+      ++$count;
+    }
+  }
+  warn sprintf "There are %s planets and %s distances\n", scalar( $pw->Planets ), $count;
+  #x 'distance', $distance;
+  return $distance;
+}
+
+sub OrderNeighbors {
+  my($pw, $distance) = @_;
+  my $neighbor;
+  #my @planetids = map $_->PlanetID(), $pw->Planets;
+
+  #x 'distance', $distance;
+
+  for my $p1 ( $pw->Planets ) {
+    my @order = 
+      map $_->[0],
+      sort { $a->[1] <=> $b->[1] }
+      map {
+        # Find distance to each planet
+        my @index = sort { $a <=> $b } ( $p1->PlanetID(), $_->PlanetID() );
+        my $dist = $distance->{$index[0]}{$index[1]};
+        #warn sprintf "Distance between %s and %s is %s\n", @index, $dist;
+        [ $_, $dist ];
+      }
+      grep { $p1->PlanetID() != $_->PlanetID }
+      $pw->Planets;
+      #@planetids;
+    $neighbor->{$p1->PlanetID()} = \@order;
+  }
+  return $neighbor;
 }
