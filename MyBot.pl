8a0c352551f1af8e295eb37b3c9789473635506f
Soren Dossing <netcom@sauber.net>
2010-09-23 13:28:57 +0900

    Common code for findind and ranking candidates in different phases of game

--- MyBot.pl
+++ MyBot.pl
@@ -91,17 +91,21 @@ sub DoTurn {
   my($pw,$session) = @_;
 
   # Opening Move
-  if ( $session->{move} == 1 ) {
+  #if ( $session->{move} == 1 ) {
     FirstMove($pw,$session);
   # I will win
+
   # Attack to erase opponent
+  #} elsif ( Balance($pw) > 1 ) {
+  #} else {
+  #  Attack($pw,$session);
+
   # Growth
+  #} else {
+  #  Grow($pw,$session);
+  #}
   # Defensive
   # I will loose
-
-  } else {
-    Grow($pw,$session);
-  }
 }
 
 sub FirstMove {
@@ -110,37 +114,18 @@ sub FirstMove {
 
   my($myplanet   )= $pw->MyPlanets();
   my $myid        = $myplanet->PlanetID();
-  my($enemyplanet)= $pw->EnemyPlanets();
-  my $enemyid     = $enemyplanet->PlanetID();
   my $numships    = $myplanet->NumShips();
 
-  # Find target closer to me than to enemy
-  my %target = Targets($pw);
-  for my $dest ( keys %target ) {
-    my $mydist    =
-      $distance->{$myid}{$dest} ||= $pw->Distance( $myid, $dest );
-    my $enemydist =
-      $distance->{$enemyid}{$dest} ||= $pw->Distance( $enemyid, $dest );
-    if ( $mydist < $enemydist ) {
-      # If closer to me than to enemy, determine the desire
-      my $planet = $pw->GetPlanet($dest);
-      my $numships = $planet->NumShips();
-      my $desire = $planet->GrowthRate() / ( $mydist + $numships );
-      $target{$dest} = { 
-        desire => $desire,
-        numships => $numships,
-      };
-    } else {
-      delete $target{$dest};
-    }
-  }
+  # Get ranked targets
+  my %target = FirstTargets($pw,$session);
+  my @moves = RankTargets($pw,$session,$myplanet,%target);
+  #x 'firstmoves', \@moves;
 
   # Send ships to higest desire as long as we have enough ships
-  for my $dest ( sort { $target{$b}{desire} <=> $target{$a}{desire} }
-                 keys %target ) {
-    my $tosend = $target{$dest}{numships}+1;
+  for my $dest ( @moves ) {
+    my $tosend = $dest->{numships}+1;
     next if $tosend >= $numships;
-    $pw->IssueOrder($myid,$dest, $tosend);
+    $pw->IssueOrder($myid,$dest->{planetid}, $tosend);
     $numships -= $tosend;
   }
 }
@@ -194,23 +179,129 @@ sub Grow {
     }
   }
 }
-# List of planets that are
-#  - not mine
-#  - mine under attack
-#
-sub Targets {
-  my $pw = shift;
 
-  # Targets are planets that are not mine, or mine that are attacked
-  my %target = map {( $_->PlanetID() => 1 )} $pw->NotMyPlanets();
-  for my $fl ( $pw->EnemyFleets() ) {
-    #next if $fl->Owner() == 1;
-    #my $planetid = $fl->DestinationPlanet()->PlanetID();
-    my $planetid = $fl->DestinationPlanet();
-    #my $planetid = 1;
-    $target{$planetid} = 1;
+sub Attack {
+  my($pw,$session) = @_;
+
+  # Targets
+  my %target = AttackTargets($pw,$session);
+
+  # Check what each planet can attack
+  for my $myplanet ( $pw->MyPlanets() ) {
+    my $myid        = $myplanet->PlanetID();
+    my $numships    = $myplanet->NumShips();
+
+    # Get ranked targets
+    my @moves = RankTargets($pw,$session,$myplanet,%target);
+
+    # Send ships to higest desire as long as we have enough ships
+    for my $dest ( @moves ) {
+      my $tosend = $dest->{numships}+1;
+      next if $tosend >= $numships;
+      $pw->IssueOrder($myid,$dest->{planetid}, $tosend);
+      $numships -= $tosend;
+    }
+    last;
+  }
+}
+
+# How attractive it is for one planet to attack another
+#   Desire = Growth / ( Distance + Ships )
+#
+sub Desire {
+  my($pw,$session,$p1,$p2) = @_;
+
+  my $p1id = $p1->PlanetID();
+  my $p2id = $p2->PlanetID();
+  my $dist = $session->{distance}{$p1id}{$p2id} ||= $pw->Distance( $p1,$p2 );
+  return $p2->GrowthRate / ( $dist + $p2->NumShips );
+}
+
+# Rank all targets by desire from one planet
+#
+sub RankTargets {
+  my($pw,$session,$myplanet,%target) = @_;
+
+  while ( my($id,$dest) = each %target ) {
+    my $planet = $dest->{planet};
+    $dest->{desire}   = Desire($pw,$session,$myplanet,$planet);
+    $dest->{numships} = $planet->NumShips();
+    $dest->{planetid} = $id;
+   }
+  return map $target{$_}, sort { $target{$b}{desire} <=> $target{$a}{desire} } keys %target;
+}
+
+# Neutral planets closer to me than to enemy
+#
+sub FirstTargets {
+  my($pw,$session) = @_;
+
+  my $distance = $session->{distance};
+  my($myplanet   )= $pw->MyPlanets();
+  my $myid        = $myplanet->PlanetID();
+  my($enemyplanet)= $pw->EnemyPlanets();
+  my $enemyid     = $enemyplanet->PlanetID();
+
+  my %target = map {( $_->PlanetID() => { planet => $_ } )} 
+               $pw->NeutralPlanets();
+  for my $dest ( keys %target ) {
+    my $mydist    =
+      $distance->{$myid}{$dest} ||= $pw->Distance( $myid, $dest );
+    my $enemydist =
+      $distance->{$enemyid}{$dest} ||= $pw->Distance( $enemyid, $dest );
+    delete $target{$dest} if $mydist >= $enemydist;
   }
   return %target;
+}
+
+# Enemy planets and planets under attack by enemy
+#
+sub AttackTargets {
+  my $pw = shift;
+
+  my %target = map {( $_->PlanetID() => { planet => $_ } )} 
+               $pw->EnemyPlanets();
+  for my $fl ( $pw->EnemyFleets() ) {
+    my $planetid = $fl->DestinationPlanet();
+    $target{$planetid}{planet} ||= $pw->GetPlanet($planetid);
+  }
+  return %target;
+}
+
+# Enemy and Neutral and Mine under attack
+#
+sub GrowTargets {
+  my $pw = shift;
+
+  my %target = map {( $_->PlanetID() => { planet => $_ } )} 
+               $pw->NoMyPlanets();
+  for my $fl ( $pw->EnemyFleets() ) {
+    my $planetid = $fl->DestinationPlanet();
+    $target{$planetid}{planet} ||= $pw->GetPlanet($planetid);
+  }
+  return %target;
+}
+
+# How much ahead/behind am I. behind < 1 < ahead
+#
+sub Balance {
+  my($pw) = @_;
+
+  my $myships = 0;
+  my $mygrowth = 0;
+  for my $planet ( $pw->MyPlanets() ) {
+    $myships += $planet->NumShips();
+    $mygrowth += $planet->GrowthRate();
+  }
+
+  my $enemyships = 0;
+  my $enemygrowth = 0;
+  for my $planet ( $pw->EnemyPlanets() ) {
+    $enemyships += $planet->NumShips();
+    $enemygrowth += $planet->GrowthRate();
+  }
+
+  return ( $myships/$enemyships + $mygrowth/$enemygrowth ) / 2;
 }
 
 sub old_CalcDistances {
