c22051d43cc659f6aad45f17454b816c552ece68
Soren Dossing <netcom@sauber.net>
2010-11-05 11:16:29 +0900

    Change to my username

--- play_tcp.sh
+++ play_tcp.sh
@@ -5,9 +5,9 @@ unless (-x "tools/tcp") {
     system("gcc -O2 -o tools/tcp tools/tcp.c");
 }
 
-die "Please set an arbitrary username and password below.\n";
+#die "Please set an arbitrary username and password below.\n";
 
 for (1..3) {
-    system("./tools/tcp 72.44.46.68 995 anonymous -p p4ssw0rd ./MyBot.pl");
+    system("./tools/tcp 72.44.46.68 995 sauber ./MyBot.pl");
     sleep 5;
 }
